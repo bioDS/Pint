9eb221d4d9a09dee1e763de5df18f6f17bc18fc2
Kieran Elmes <kelmes@cs.otago.ac.nz>
2019-05-08 18:10:30 +1200

    make get_num available for lasso_exe and tests

--- src/lasso_lib.h
+++ src/lasso_lib.h
@@ -45,3 +45,4 @@ double update_beta_glmnet(int **X, double *Y, int n, int p, double lambda, doubl
 double soft_threshold(double z, double gamma);
 double *read_y_csv(char *fn, int n);
 XMatrix read_x_csv(char *fn, int n, int p);
+int_pair get_num(int num, int p);
