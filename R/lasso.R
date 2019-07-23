df64f1bdddf16d576c0fd3d0a59ea3c74d84891b
Kieran Elmes <=>
2019-07-23 18:52:35 +1200

    wip: R packaging; installable but not working

--- R/lasso.R
+++ R/lasso.R
@@ -1,4 +1,15 @@
-CyclicLasso <- function(xName, yName, lambda, n, p) {
+#' @title Cyclic Lasso Function
+#'
+#' @description Performas a cyclic lasso
+#' @param X_filename, Y_filename, lambda, n, p
+#' @export
+#' @examples
+#' cyclic_lasso(xName = "X.csv", yName = "Y.csv", lambda = 20, n = 1000, p = 100)
+
+
+dyn.load("build/src/.libs/liblassoPackage.so.0")
+cyclic_lasso <- function(xName = "X.csv", yName = "Y.csv", lambda = 20, n = 1000, p = 100) {
     Ret = .C("lasso", as.character(xName), as.character(yName), as.numeric(lambda), as.integer(n), as.integer(p))
+    #Ret = "test"
     return(Ret)
 }
