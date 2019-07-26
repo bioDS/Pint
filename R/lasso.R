dc492c127db7f1752e52da12fb064d8c7272af83
Kieran Elmes <kelmes@cs.otago.ac.nz>
2019-07-26 15:44:54 +1200

    check arguments are valid in R

--- R/lasso.R
+++ R/lasso.R
@@ -8,9 +8,15 @@
 #' cyclic_lasso(xName = "X.csv", yName = "Y.csv", lambda = 20, n = 1000, p = 100)
 #' @useDynLib LassoTesting
 
-cyclic_lasso <- function(xName = "X.csv", yName = "Y.csv", lambda = 20, n = 1000, p = 100) {
+cyclic_lasso <- function(xName = "X.csv", yName = "Y.csv", lambda = 20, n = 1000, p = 100, frac_overlap_allowed = 0.1) {
+    if (!file.exists(xName)) {
+        stop("X file does not exist")
+    }
+    if (!file.exists(yName)) {
+        stop("Y file does not exist")
+    }
     #dyn.load(LassoTesting)
-    Ret = .Call(lasso_, as.character(xName), as.character(yName), as.numeric(lambda), as.integer(n), as.integer(p))
+    Ret = .Call(lasso_, as.character(xName), as.character(yName), as.numeric(lambda), as.integer(n), as.integer(p), as.numeric(frac_overlap_allowed))
     #Ret = "test"
     return(Ret)
 }
