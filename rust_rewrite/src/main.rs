8399a1970f64a6ac3779a17e30cf442c1f9a87a0
Kieran Elmes <kelmes@caelus.nz>
2020-01-08 19:30:20 +1300

    wip: use bitpacking

--- rust_rewrite/src/main.rs
+++ rust_rewrite/src/main.rs
@@ -1,11 +1,89 @@
 use std::fs::File;
 use clap::{Arg, App, SubCommand};
+use bitpacking::{BitPacker8x, BitPacker};
+use rand::{thread_rng, Rng};
+use std::cmp::min;
 
 //TODO: use bitpacking
 //TODO: multithreading
+//TODO: replace actual_entries with final block
 
 struct XMatrix {
     rows: Vec<Vec<bool>>
+}
+
+struct Compressed_Column {
+    bytes: Vec<u8>,
+    block_inf: Vec<(usize, u8, usize)>, //compressed_len, num_bits, actual_entries
+    size: usize,
+}
+
+struct ColumnIterator<'a> {
+    column: &'a Compressed_Column,
+    index: usize,
+    bitpacker: BitPacker8x,
+    last_value: u32,
+    prev_pos: usize,
+}
+
+impl Iterator for ColumnIterator<'_> {
+    type Item = Vec<u32>;
+
+    fn next(&mut self) -> Option<Vec<u32>> {
+        if self.index == self.column.block_inf.len() {
+            return None;
+        }
+        //println!("doing something");
+        let bitpacker = self.bitpacker;
+        let mut col_ind = 0;
+        assert!(self.column.block_inf.len() > 0);
+        let (compressed_len,num_bits, actual_entries) = self.column.block_inf[self.index];
+        //println!("len: {}, bits: {}, actual_entries {}", compressed_len, num_bits, actual_entries);
+        let mut decompressed = vec![0_u32; BitPacker8x::BLOCK_LEN];
+        //println!("reading from {}. {} entries. starting at position {} in bytes. reading {} bytes", self.last_value, actual_entries, self.prev_pos, compressed_len);
+        bitpacker.decompress_sorted(self.last_value, &self.column.bytes[self.prev_pos..self.prev_pos+compressed_len], &mut decompressed[..], num_bits);
+        //print!("old prev_pos: {}", self.prev_pos);
+        self.prev_pos += compressed_len;
+        //println!(", new prev_pos: {}", self.prev_pos);
+        self.index += 1;
+        self.last_value = decompressed[actual_entries - 1];
+        decompressed.resize(actual_entries, 0);
+        //println!("new last value: {}", self.last_value);
+        //println!("first value: {} last value {}", decompressed[0], self.last_value);
+        Some(decompressed)
+    }
+}
+
+//impl<'a> IntoIterator for Compressed_Column {
+//    type Item = Vec<u32>;
+//    type IntoIter = ColumnIterator<'a>;
+//
+//    fn into_iter(self) -> ColumnIterator<'a> {
+//        //println!("self.block_inf.len() {}, size {}", self.block_inf.len(), self.size);
+//        ColumnIterator {
+//            column: &self,
+//            index: 0,
+//            bitpacker: BitPacker8x::new(),
+//            last_value: 0,
+//            prev_pos: 0
+//        }
+//    }
+//}
+
+impl<'a> IntoIterator for &'a Compressed_Column {
+    type Item = Vec<u32>;
+    type IntoIter = ColumnIterator<'a>;
+
+    fn into_iter(self) -> ColumnIterator<'a> {
+        //println!("self.block_inf.len() {}, size {}", self.block_inf.len(), self.size);
+        ColumnIterator {
+            column: self,
+            index: 0,
+            bitpacker: BitPacker8x::new(),
+            last_value: 0,
+            prev_pos: 0
+        }
+    }
 }
 
 struct XMatrix_Cols {
@@ -15,8 +93,8 @@ struct XMatrix_Cols {
 struct Sparse_Xmatrix {
     n: usize,
     p: usize,
-    column_indices: Vec<Vec<usize>>,
-    column_nonzeros: Vec<usize>
+    compressed_columns: Vec<Compressed_Column>,
+    bitpacker: bitpacking::BitPacker8x
 }
 
 trait matrix_functions {
@@ -40,15 +118,92 @@ mod tests {
         let X2 = x_to_x2_sparse_col(&X_col);
 
         let testX2 = read_x_csv("./testX2.csv");
-        for col_ind in 0..X2.column_indices.len() {
-            let col = &X2.column_indices[col_ind];
-            for entry in col {
-                if !testX2.rows[*entry][col_ind] {
-                    println!("col {} row {} not present in testX2.csv", col_ind, entry);
+        let mut actual_count: usize = 0;
+        for row in &testX2.rows {
+            let size: usize = row.iter().map(|entry| if *entry==true {1} else {0}).sum();
+            actual_count += size;
+        }
+        let bitpacker = X2.bitpacker;
+        let mut col_ind = 0;
+        let mut count = 0;
+        for column in X2.compressed_columns {
+            for block in &column {
+                for entry in block {
+                    if !testX2.rows[entry as usize][col_ind] {
+                        println!("col {} row {} not present in testX2.csv (count {})", col_ind, entry, count);
+                    }
+                    assert!(testX2.rows[entry as usize][col_ind]);
+                    count += 1;
                 }
-                assert!(testX2.rows[*entry][col_ind]);
+            }
+
+            col_ind += 1;
+        }
+        assert_eq!(count, actual_count);
+        assert_eq!(col_ind, (X_col.cols.len()*(X_col.cols.len()+1))/2);
+    }
+
+    #[test]
+    fn test_bitpacking() {
+        let mut rng = rand::thread_rng();
+        let mut my_data: Vec<u32> = Vec::new();
+        let n = 513;
+        //let my_data: Vec<u32> = (0..n).collect();
+        for _i in 0..n {
+            my_data.push(my_data.last().unwrap_or(&0_u32) + rng.gen_range(0,100));
+        }
+        let bitpacker = BitPacker8x::new();
+        let block_len = BitPacker8x::BLOCK_LEN;
+        let mut compressed_col: Vec<u8> = Vec::new();
+        let mut compressed_lens = Vec::new();
+
+        println!("bing!");
+        // add elements until we have less than one chunk left
+        if n % block_len != 0 {
+            my_data.resize((n as usize/block_len + 1)*block_len, *my_data.last().unwrap_or(&0));
+        }
+        println!("new len: {}", my_data.len());
+        let mut last_value = 0;
+        for ind in 0..(my_data.len() as usize/block_len) {
+            let actual_entries = min(n - ind*block_len, block_len);
+            println!("actual_entries: {}", actual_entries);
+            let num_bits: u8 = bitpacker.num_bits(&my_data[ind*block_len..(ind+1)*block_len]);
+            let mut compressed = vec![0_u8; 8*BitPacker8x::BLOCK_LEN];
+            let compressed_len = bitpacker.compress_sorted(last_value, &my_data[ind*block_len..(ind+1)*block_len], &mut compressed[..], num_bits);
+            last_value = my_data[ind*block_len + actual_entries - 1];
+            println!("new last value: {}", last_value);
+            println!("adding ({},{})", compressed_len, num_bits);
+            compressed_lens.push((compressed_len, num_bits, actual_entries));
+            compressed_col.extend_from_slice(&mut compressed[0..compressed_len]);
+        }
+
+
+        let mut prev_pos = 0;
+        let mut count = 0;
+        let mut found = 0;
+        let mut last_value = 0;
+        for (compressed_len, num_bits, actual_entries) in &compressed_lens {
+            println!("len: {}, bits: {}", compressed_len, num_bits);
+            let mut decompressed = vec![0_u32; BitPacker8x::BLOCK_LEN];
+            println!("prev_pos: {}", prev_pos);
+            bitpacker.decompress_sorted(last_value, &compressed_col[prev_pos..prev_pos+compressed_len], &mut decompressed[..], *num_bits);
+            last_value = decompressed[actual_entries - 1];
+            println!("new last value: {}", last_value);
+            prev_pos += compressed_len;
+
+            assert_eq!(&my_data[count..count+*actual_entries], &decompressed[..*actual_entries]);
+            found += actual_entries;
+            count += block_len;
+        }
+        count = 0;
+        let cc = Compressed_Column { size: n, bytes: compressed_col, block_inf: compressed_lens };
+        for block in &cc {
+            for entry in block {
+                assert_eq!(entry, my_data[count]);
+                count += 1;
             }
         }
+        assert_eq!(found, n);
     }
 }
 
@@ -73,12 +228,14 @@ fn main() {
     let y_filename = matches.value_of("ymatrix").unwrap_or("testY.csv");
 
     let X_row = read_x_csv(x_filename);
+    print!("X is {}x{}, ", X_row.rows.len(), X_row.rows[0].len());
     let X_col = row_to_col(X_row);
     let Y = read_y_csv(y_filename);
+    println!("Y is {}", Y.len());
 
     let X2 = x_to_x2_sparse_col(&X_col);
 
-    let beta = simple_coordinate_descent_lasso(&X2, Y);
+    let beta = simple_coordinate_descent_lasso(X2, Y);
 
     // find large looking beta values
     for b_ind in 0..beta.len() {
@@ -98,27 +255,53 @@ fn x_to_x2_sparse_col(X: &XMatrix_Cols) -> Sparse_Xmatrix {
     let p_int = (X.cols.len()*(X.cols.len()+1))/2;
     let n = X.cols[0].len();
     println!("building X2. n = {}, p = {}", n, p);
-    let mut column_indices: Vec<Vec<usize>> = vec![Vec::new(); p_int];
-    let mut column_nonzeros = vec![0; p_int];
     let mut col_ind = 0;
+
+    let bitpacker = BitPacker8x::new();
+    let block_len = BitPacker8x::BLOCK_LEN;
+    let mut compressed_columns: Vec<Compressed_Column> = Vec::new();
+
     for col1_ind in 0..p {
         for col2_ind in col1_ind..p {
+            let mut current_col_indices: Vec<u32> = Vec::new();
             let col1 = &X.cols[col1_ind];
             let col2 = &X.cols[col2_ind];
-            //let size: usize = col.iter().map(|entry| if *entry==true {1} else {0}).sum();
             let mut size = 0;
             for k in 0..n {
                 if col1[k] == true && col2[k] == true{
                     size += 1;
-                    column_indices[col_ind].push(k);
+                    current_col_indices.push(k as u32);
                 }
             }
-            column_nonzeros[col_ind] = size;
+
+            let mut compressed_col: Vec<u8> = Vec::new();
+            let mut compressed_lens: Vec<(usize, u8, usize)> = Vec::new();
+            let mut last_value = 0;
+            if current_col_indices.len() % BitPacker8x::BLOCK_LEN != 0 {
+                current_col_indices.resize((current_col_indices.len()/BitPacker8x::BLOCK_LEN +1)*BitPacker8x::BLOCK_LEN, *current_col_indices.last().unwrap_or(&0));
+            }
+
+            for ind in 0..(current_col_indices.len() as usize/block_len) {
+                let actual_entries = min(size - ind*block_len, block_len);
+                let num_bits: u8 = bitpacker.num_bits(&current_col_indices[ind*block_len..(ind+1)*block_len]);
+                let mut compressed = vec![0_u8; 8*BitPacker8x::BLOCK_LEN];
+                let compressed_len = bitpacker.compress_sorted(last_value, &current_col_indices[ind*block_len..(ind+1)*block_len], &mut compressed[..], num_bits);
+                last_value = current_col_indices[ind*block_len + actual_entries - 1];
+                //println!("new last value: {}", last_value);
+
+                //println!("adding ({},{})", compressed_len, num_bits);
+                compressed_lens.push((compressed_len, num_bits, actual_entries));
+                compressed_col.extend_from_slice(&mut compressed[0..compressed_len]);
+            }
+
+            //println!("pushing size {}, lens {}", size, compressed_lens.len());
+            compressed_columns.push(Compressed_Column { bytes: compressed_col, block_inf: compressed_lens, size});
+
             col_ind += 1;
         }
     }
 
-    Sparse_Xmatrix { n, p: p_int, column_indices, column_nonzeros }
+    Sparse_Xmatrix { n, p: p_int, compressed_columns, bitpacker }
 }
 
 fn row_to_col(row_X: XMatrix) -> XMatrix_Cols {
@@ -170,29 +353,32 @@ fn X2_from_X(X: XMatrix) -> Vec<Vec<bool>> {
 }
 
 // X should be column major
-fn simple_coordinate_descent_lasso(X: &Sparse_Xmatrix, Y: Vec<f64>)  -> Vec<f64> {
+fn simple_coordinate_descent_lasso(X: Sparse_Xmatrix, Y: Vec<f64>)  -> Vec<f64> {
     let p = X.p;
     let n = X.n;
     println!("p: {}, n: {}", p, n);
     let mut beta = vec![0.0; p];
-    let mut rowsums = vec![0.0; n];
+    let mut rowsum = vec![0.0; n];
     let mut colsums = vec![0; p];
     let mut lambda = 100.0; //TODO: find max lambda;
     let halt_beta_diff = 1.0001;
-    let mut error = calculate_error(&rowsums, &Y);
+    let mut error = calculate_error(&rowsum, &Y);
     println!("for testing purposes, initial e is {:.2}", error as f64);
+    let mut column_iter: Vec<ColumnIterator> = Vec::new();
+    //for i in 0..X.compressed_columns.len() {
+    //    column_iter.push(X.compressed_columns[i].into_iter());
+    //}
     for lambda_seq in 0..50 {
         println!("lambda {}: {}", lambda_seq+1, lambda);
         for iter in 0..100 {
             let mut iter_max_change = 0.0;
-            for k in 0..p {
-                let col_k_max_change = update_beta_cyclic(X, &Y, &mut beta, n, p, k, &mut rowsums, lambda);
-                if col_k_max_change > iter_max_change {
-                    iter_max_change = col_k_max_change;
-                }
+            let mut k = 0;
+            //for (col) in &column_iter {
+            for (k,column) in X.compressed_columns.iter().enumerate() {
+                update_beta_cyclic(&column, &Y, &mut beta, n, p, &mut rowsum, lambda, k);
             }
             let prev_error = error;
-            error = calculate_error(&rowsums, &Y);
+            error = calculate_error(&rowsum, &Y);
             //println!("error: {}", error);
             if prev_error/error < halt_beta_diff {
                 println!("Change was {:.2}. Halting after {} iterations. e: {:.2}", (prev_error/error), iter, error as f64);
@@ -229,13 +415,17 @@ fn calculate_error(rowsums: &Vec<f64>, Y: &Vec<f64>) -> f64 {
     error
 }
 
-fn update_beta_cyclic(X: &Sparse_Xmatrix, Y: &Vec<f64>, beta: &mut Vec<f64>, n: usize, p: usize, k: usize, rowsum: &mut Vec<f64>, lambda: f64) -> f64 {
-    let sumk = X.column_nonzeros[k] as f64;
-    let mut sumn = X.column_nonzeros[k] as f64 * beta[k];
-    for i in &X.column_indices[k] {
-        sumn += Y[*i] - rowsum[*i] as f64;
-    }
+fn update_beta_cyclic(column: &Compressed_Column, Y: &Vec<f64>, beta: &mut Vec<f64>, n: usize, p: usize, rowsum: &mut Vec<f64>, lambda: f64, k: usize) {
+    let sumk = column.size as f64;
+    let mut sumn = sumk * beta[k];
+    let mut complete_row: Vec<usize> = Vec::with_capacity(column.size);
     let old_beta_k = beta[k];
+    for block in column.into_iter() {
+        for entry in block {
+            sumn += Y[entry as usize] - rowsum[entry as usize] as f64;
+            complete_row.push(entry as usize);
+        }
+    }
     if sumk == 0.0 {
         beta[k] = 0.0;
     } else {
@@ -244,13 +434,10 @@ fn update_beta_cyclic(X: &Sparse_Xmatrix, Y: &Vec<f64>, beta: &mut Vec<f64>, n: 
     let beta_k_diff = beta[k] - old_beta_k;
     // update rowsums if we have to
     if beta_k_diff != 0.0 {
-        for i in &X.column_indices[k] {
-            rowsum[*i] += beta_k_diff;
+        for i in complete_row {
+            rowsum[i] += beta_k_diff;
         }
     }
-    let squared_beta_k_diff = beta_k_diff*beta_k_diff;
-
-    return squared_beta_k_diff;
 }
 
 fn soft_threshold(z: f64, gamma: f64) -> f64 {
@@ -290,6 +477,8 @@ fn read_x_csv(file_path: &str) -> XMatrix {
                 else if val == 0 { false }
                 else { panic!("read invalid value"); })
         }
+        // ensure all rows are the same size
+        assert_eq!(new_row.len(), rows.last().unwrap_or(&new_row).len());
         rows.push(new_row);
     }
 
