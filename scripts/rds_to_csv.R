0866020f5ffd50f71604008486427e6b9b3c4a79
Kieran Elmes <kelmes@cs.otago.ac.nz>
2020-04-07 15:29:41 +1200

    output csvs to separate subdirectories

--- scripts/rds_to_csv.R
+++ scripts/rds_to_csv.R
@@ -10,11 +10,11 @@ output_dir = args[2]
 base_filename = sub("\\..*", "", sub(".*/", "", input_file))
 data = readRDS(input_file)
 
-x_csv_filename = paste(sep="", output_dir, "/", base_filename, "_X.csv")
-y_csv_filename = paste(sep="", output_dir, "/", base_filename, "_y.csv")
+output_file_path = paste(sep="", output_dir, "/", base_filename, "/")
+dir.create(output_file_path)
 
-print(paste("writing to ", x_csv_filename))
-write.table(data$X, file=x_csv_filename, row.names=T, col.names=F, sep=',')
+print(paste("writing to ", paste(output_file_path, sep="", "X.csv")))
+write.table(data$X, file=paste(output_file_path, sep="", "X.csv"), row.names=T, col.names=F, sep=',')
 
-print(paste("writing to ", y_csv_filename))
-write.table(data$Y, file=y_csv_filename, row.names=T, col.names=F, sep=',')
+print(paste("writing to ", paste(output_file_path, sep="", "Y.csv")))
+write.table(data$Y, file=paste(output_file_path, sep="", "Y.csv"), row.names=T, col.names=F, sep=',')
