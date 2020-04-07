d92abc78bc9a431b58ce0141003a7fbf353b0caf
Kieran Elmes <kelmes@cs.otago.ac.nz>
2020-04-07 15:34:02 +1200

    remove debug script output

--- scripts/bench_small.sh
+++ scripts/bench_small.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-set -x #echo on
+#set -x #echo on
 
 #BENCHMARK_SETS=/home/kieran/work/data/simulated_data_small_repeat
 BENCHMARK_SETS=/home/kieran/work/lasso_testing/bench_sets_small
@@ -11,18 +11,12 @@ cd $BASE_DIR
 mkdir -p "$current_run_dir"
 
 for bench_set in `ls $BENCHMARK_SETS`; do
-    echo $bench_set
+    echo "running $bench_set"
     x="$BENCHMARK_SETS/$bench_set/X.csv"
     y="$BENCHMARK_SETS/$bench_set/Y.csv"
 
-    echo "X: $x"
-    echo "Y: $y"
-
     rows=`wc -l $y | cut -d' ' -f1`
     cols=`head -n1 $x | cut -d'"' -f3- | tr ',' ' ' | wc -w`
-
-    echo "cols: $cols"
-    echo "rows: $rows"
 
     ./mergefind-release/src/lasso_exe $x $y cyclic int F 1 $rows $cols > "$OUTPUT_DIR/$bench_set.log"
 done
