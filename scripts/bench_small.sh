081f66482dd9c05d6d17388885c79b81425ede20
Kieran Elmes <kelmes@cs.otago.ac.nz>
2020-04-07 17:16:07 +1200

    fix using benchmark names

--- scripts/bench_small.sh
+++ scripts/bench_small.sh
@@ -8,16 +8,15 @@ OUTPUT_DIR=/home/kieran/work/lasso_testing/bench_runs
 
 LAMBDA_VALUE=0.1
 
-test_name=$1
-
-if [ -z test_name ]; then
+if [ -z $1 ]; then
     echo "must supply a test name as an argument"
     exit
 fi
 
+test_name=$1
+
 cd $BASE_DIR
-#current_run_dir="bench_runs/$(date)"
-mkdir -p "$current_run_dir"
+mkdir -p $OUTPUT_DIR/$test_name
 
 for bench_set in `ls $BENCHMARK_SETS`; do
     echo "running $bench_set"
@@ -27,5 +26,5 @@ for bench_set in `ls $BENCHMARK_SETS`; do
     rows=`wc -l $y | cut -d' ' -f1`
     cols=`head -n1 $x | cut -d'"' -f3- | tr ',' ' ' | wc -w`
 
-    ./mergefind-release/src/lasso_exe $x $y cyclic int F 1 $rows $cols > "$OUTPUT_DIR/$bench_set.log"
+    ./mergefind-release/src/lasso_exe $x $y cyclic int F $LAMBDA_VALUE $rows $cols > "$OUTPUT_DIR/$1/$bench_set.log"
 done
