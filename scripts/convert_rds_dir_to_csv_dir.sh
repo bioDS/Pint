b17d9eabded127486cc3bc81238e5948f095620a
Kieran Elmes <kelmes@caelus.nz>
2020-04-16 16:42:54 +1200

    allow limiting the number of .rds files to convert to .csv

--- scripts/convert_rds_dir_to_csv_dir.sh
+++ scripts/convert_rds_dir_to_csv_dir.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -21,8 +21,22 @@ if [ ! -d $2 ]; then
     exit
 fi;
 
+if [ ! -z $3 ]; then
+    limit=$3
+else
+    ((limit=-1))
+fi;
+
 echo "converting from '$1' to '$2'"
 
 for file in `ls $1`; do
+    if [ $limit -gt 0 ]; then
+        echo "$limit iterations remaining"
+    fi
     $SCRIPT_DIR/rds_to_csv.R "$1/$file" $2
+    ((limit=limit-1))
+    if [ $limit -eq 0 ]; then
+        echo "halting after reaching iteration limit"
+        break;
+    fi;
 done
