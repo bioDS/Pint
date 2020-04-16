f2f8297008a0cd29b01a9b01fa32285a45e6a706
Kieran Elmes <kelmes@cs.otago.ac.nz>
2020-04-16 12:28:59 +1200

    missed line in previous commit

--- scripts/convert_rds_dir_to_csv_dir.sh
+++ scripts/convert_rds_dir_to_csv_dir.sh
@@ -24,5 +24,5 @@ fi;
 echo "converting from '$1' to '$2'"
 
 for file in `ls $1`; do
-    /home/kieran/work/lasso_testing/scripts/rds_to_csv.R "$1/$file" $2
+    $SCRIPT_DIR/rds_to_csv.R "$1/$file" $2
 done
