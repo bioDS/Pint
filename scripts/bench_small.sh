78bc6f469acd79cb909ad3f52a2a8a6f836bd467
Kieran Elmes <kelmes@cs.otago.ac.nz>
2020-04-16 12:10:12 +1200

    use bench.sh in bench_small

--- scripts/bench_small.sh
+++ scripts/bench_small.sh
@@ -5,26 +5,8 @@
 BENCHMARK_SETS=/home/kieran/work/lasso_testing/bench_sets_small
 BASE_DIR=/home/kieran/work/lasso_testing
 OUTPUT_DIR=/home/kieran/work/lasso_testing/bench_runs
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 LAMBDA_VALUE=0.1
 
-if [ -z $1 ]; then
-    echo "must supply a test name as an argument"
-    exit
-fi
-
-test_name=$1
-
-cd $BASE_DIR
-mkdir -p $OUTPUT_DIR/$test_name
-
-for bench_set in `ls $BENCHMARK_SETS`; do
-    echo "running $bench_set"
-    x="$BENCHMARK_SETS/$bench_set/X.csv"
-    y="$BENCHMARK_SETS/$bench_set/Y.csv"
-
-    rows=`wc -l $y | cut -d' ' -f1`
-    cols=`head -n1 $x | cut -d'"' -f3- | tr ',' ' ' | wc -w`
-
-    ./mergefind-release/src/lasso_exe $x $y cyclic int F $LAMBDA_VALUE $rows $cols > "$OUTPUT_DIR/$1/$bench_set.log"
-done
+bash $SCRIPT_DIR/bench.sh $BENCHMARK_SETS $BASE_DIR $OUTPUT_DIR $1
