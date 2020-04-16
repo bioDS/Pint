2e1d14694c2df6b95fe5f4f81d29c9c4abcc6f9f
Kieran Elmes <kelmes@cs.otago.ac.nz>
2020-04-16 12:16:08 +1200

    argument checking in convert script

--- scripts/convert_rds_dir_to_csv_dir.sh
+++ scripts/convert_rds_dir_to_csv_dir.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/bash
 
+
+if [ -z $1 ]; then
+    echo "usage convert_rds_dir_to_csv_dir.sh [from] [to]"
+    echo "must supply from directory"
+    exit
+fi
+if [ ! -d $1 ]; then
+    echo "from directory must exist"
+    exit
+fi;
+if [ -z $2 ]; then
+    echo "usage convert_rds_dir_to_csv_dir.sh [from] [to]"
+    echo "must supply to directory"
+    exit
+fi
+if [ ! -d $2 ]; then
+    echo "to directory must exist"
+    exit
+fi;
+
 echo "converting from '$1' to '$2'"
 
 for file in `ls $1`; do
