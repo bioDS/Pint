ef665013b251d9f2f73e77d61f52167c3839c8c6
Kieran Elmes <kelmes@cs.otago.ac.nz>
2020-08-04 17:02:42 +1200

    specify nixpkgs version, build should now be actually reproducable

--- shell.nix
+++ shell.nix
@@ -1,11 +1,13 @@
 let
-	pkgs = import <nixpkgs> {};
+	pkgs = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/20.03.tar.gz) {};
 in
 pkgs.stdenv.mkDerivation {
 	name = "my-env";
 	buildInputs = [
 		pkgs.pkgconfig
 		pkgs.R
+		pkgs.rPackages.dplyr
+		pkgs.rPackages.Matrix
 		pkgs.gcc9
 		pkgs.gsl
 		pkgs.glib.dev
